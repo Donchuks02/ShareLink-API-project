43dc39fba73edc5dda9debccc8126209acd6e468
Donchuks02 <chukwudidavid02@gmail.com>
2024-09-26 10:30:04 +0100

    added allowed hosts

--- projectConfig/settings.py
+++ projectConfig/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'sharelink-api.onrender.com']
 
 # Application definition
 
