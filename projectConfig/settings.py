f9f0d8d76460d1a4651f41d2fe178a3f2832030d
Donchuks02 <chukwudidavid02@gmail.com>
2024-09-26 05:09:33 +0100

    installed and added environment variables

--- projectConfig/settings.py
+++ projectConfig/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env
 
+env = Env()
+env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
